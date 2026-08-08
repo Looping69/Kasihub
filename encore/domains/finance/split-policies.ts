63b4040b382059aaa8a0fcc7dfd1e03e916e723a
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-08-09 00:04:20 +0200

    refactor(finance): classify split recipients explicitly

--- encore/domains/finance/split-policies.ts
+++ encore/domains/finance/split-policies.ts
@@ -6,11 +6,11 @@ export const ZAR_MINOR_UNIT_SCALE = 2;
 export const CUSTODIAN_REMAINDER_RULE = "custodian";
 
 const adultProfitRules = [
-  { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, basisPoints: 5900 },
-  { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", basisPoints: 100 },
-  { code: "private_pool", recipientType: "PRIVATE_POOL", basisPoints: 100 },
-  { code: "npo_pool", recipientType: "NPO_POOL", basisPoints: 100 },
-  { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", basisPoints: 3800 },
+  { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, recipientMode: "system", basisPoints: 5900 },
+  { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", recipientMode: "system", basisPoints: 100 },
+  { code: "private_pool", recipientType: "PRIVATE_POOL", recipientMode: "system", basisPoints: 100 },
+  { code: "npo_pool", recipientType: "NPO_POOL", recipientMode: "system", basisPoints: 100 },
+  { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", recipientMode: "system", basisPoints: 3800 },
 ] as const;
 
 export const adultMembershipProfitPolicyV1: SplitPolicy = {
@@ -51,13 +51,13 @@ export const npoNgoCampaignPolicyV1: SplitPolicy = {
   minorUnitScale: ZAR_MINOR_UNIT_SCALE,
   remainderRuleCode: CUSTODIAN_REMAINDER_RULE,
   rules: [
-    { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, basisPoints: 4200 },
-    { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", basisPoints: 100 },
-    { code: "private_pool", recipientType: "PRIVATE_POOL", basisPoints: 100 },
-    { code: "npo_pool", recipientType: "NPO_POOL", basisPoints: 100 },
-    { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", basisPoints: 2500 },
-    { code: "group_referrer", recipientType: "GROUP_REFERRER", basisPoints: 1500 },
-    { code: "referrer_npo", recipientType: "REFERRER_NPO", basisPoints: 1500 },
+    { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, recipientMode: "system", basisPoints: 4200 },
+    { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "private_pool", recipientType: "PRIVATE_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "npo_pool", recipientType: "NPO_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", recipientMode: "system", basisPoints: 2500 },
+    { code: "group_referrer", recipientType: "GROUP_REFERRER", recipientMode: "dynamic", basisPoints: 1500 },
+    { code: "referrer_npo", recipientType: "REFERRER_NPO", recipientMode: "dynamic", basisPoints: 1500 },
   ],
 };
 
@@ -69,14 +69,14 @@ export const marketplaceProductPolicyV1: SplitPolicy = {
   minorUnitScale: ZAR_MINOR_UNIT_SCALE,
   remainderRuleCode: CUSTODIAN_REMAINDER_RULE,
   rules: [
-    { code: "member_cashback", recipientType: "MEMBER_CASHBACK", basisPoints: 1000 },
-    { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, basisPoints: 2700 },
-    { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", basisPoints: 100 },
-    { code: "private_pool", recipientType: "PRIVATE_POOL", basisPoints: 100 },
-    { code: "npo_pool", recipientType: "NPO_POOL", basisPoints: 100 },
-    { code: "marketplace_pool", recipientType: "KASI_MARKETPLACE_POOL", basisPoints: 2500 },
-    { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", basisPoints: 2500 },
-    { code: "referrer", recipientType: "REFERRER", basisPoints: 1000 },
+    { code: "member_cashback", recipientType: "MEMBER_CASHBACK", recipientMode: "dynamic", basisPoints: 1000 },
+    { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, recipientMode: "system", basisPoints: 2700 },
+    { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "private_pool", recipientType: "PRIVATE_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "npo_pool", recipientType: "NPO_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "marketplace_pool", recipientType: "KASI_MARKETPLACE_POOL", recipientMode: "system", basisPoints: 2500 },
+    { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", recipientMode: "system", basisPoints: 2500 },
+    { code: "referrer", recipientType: "REFERRER", recipientMode: "dynamic", basisPoints: 1000 },
   ],
 };
 
@@ -88,15 +88,15 @@ export const productCampaignGroupPolicyV1: SplitPolicy = {
   minorUnitScale: ZAR_MINOR_UNIT_SCALE,
   remainderRuleCode: CUSTODIAN_REMAINDER_RULE,
   rules: [
-    { code: "member_cashback", recipientType: "MEMBER_CASHBACK", basisPoints: 1000 },
-    { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, basisPoints: 2300 },
-    { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", basisPoints: 100 },
-    { code: "private_pool", recipientType: "PRIVATE_POOL", basisPoints: 100 },
-    { code: "npo_pool", recipientType: "NPO_POOL", basisPoints: 100 },
-    { code: "marketplace_pool", recipientType: "KASI_MARKETPLACE_POOL", basisPoints: 2200 },
-    { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", basisPoints: 2200 },
-    { code: "campaign_referrer", recipientType: "CAMPAIGN_REFERRER", basisPoints: 1000 },
-    { code: "campaign_manager", recipientType: "CAMPAIGN_MANAGER", basisPoints: 1000 },
+    { code: "member_cashback", recipientType: "MEMBER_CASHBACK", recipientMode: "dynamic", basisPoints: 1000 },
+    { code: CUSTODIAN_REMAINDER_RULE, recipientType: KASIHUB_CUSTODIAN, recipientMode: "system", basisPoints: 2300 },
+    { code: "pioneer_pool", recipientType: "KASIPIONEER_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "private_pool", recipientType: "PRIVATE_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "npo_pool", recipientType: "NPO_POOL", recipientMode: "system", basisPoints: 100 },
+    { code: "marketplace_pool", recipientType: "KASI_MARKETPLACE_POOL", recipientMode: "system", basisPoints: 2200 },
+    { code: "shareholders_pool", recipientType: "KASI_SHAREHOLDERS_POOL", recipientMode: "system", basisPoints: 2200 },
+    { code: "campaign_referrer", recipientType: "CAMPAIGN_REFERRER", recipientMode: "dynamic", basisPoints: 1000 },
+    { code: "campaign_manager", recipientType: "CAMPAIGN_MANAGER", recipientMode: "dynamic", basisPoints: 1000 },
   ],
 };
 
@@ -108,12 +108,12 @@ export const ecosystemUplineR53PolicyV1: FixedSplitPolicy = {
   minorUnitScale: ZAR_MINOR_UNIT_SCALE,
   expectedTotalMinor: 5300n,
   rules: [
-    { code: "upline_level_1", recipientType: "UPLINE_LEVEL_1", amountMinor: 1300n, fallbackRecipientType: KASIHUB_CUSTODIAN },
-    { code: "upline_level_2", recipientType: "UPLINE_LEVEL_2", amountMinor: 1100n, fallbackRecipientType: KASIHUB_CUSTODIAN },
-    { code: "upline_level_3", recipientType: "UPLINE_LEVEL_3", amountMinor: 1100n, fallbackRecipientType: KASIHUB_CUSTODIAN },
-    { code: "upline_level_4", recipientType: "UPLINE_LEVEL_4", amountMinor: 900n, fallbackRecipientType: KASIHUB_CUSTODIAN },
-    { code: "upline_level_5", recipientType: "UPLINE_LEVEL_5", amountMinor: 600n, fallbackRecipientType: KASIHUB_CUSTODIAN },
-    { code: "upline_level_6", recipientType: "UPLINE_LEVEL_6", amountMinor: 300n, fallbackRecipientType: KASIHUB_CUSTODIAN },
+    { code: "upline_level_1", recipientType: "UPLINE_LEVEL_1", recipientMode: "dynamic", amountMinor: 1300n, fallbackRecipientType: KASIHUB_CUSTODIAN },
+    { code: "upline_level_2", recipientType: "UPLINE_LEVEL_2", recipientMode: "dynamic", amountMinor: 1100n, fallbackRecipientType: KASIHUB_CUSTODIAN },
+    { code: "upline_level_3", recipientType: "UPLINE_LEVEL_3", recipientMode: "dynamic", amountMinor: 1100n, fallbackRecipientType: KASIHUB_CUSTODIAN },
+    { code: "upline_level_4", recipientType: "UPLINE_LEVEL_4", recipientMode: "dynamic", amountMinor: 900n, fallbackRecipientType: KASIHUB_CUSTODIAN },
+    { code: "upline_level_5", recipientType: "UPLINE_LEVEL_5", recipientMode: "dynamic", amountMinor: 600n, fallbackRecipientType: KASIHUB_CUSTODIAN },
+    { code: "upline_level_6", recipientType: "UPLINE_LEVEL_6", recipientMode: "dynamic", amountMinor: 300n, fallbackRecipientType: KASIHUB_CUSTODIAN },
   ],
 };
 
